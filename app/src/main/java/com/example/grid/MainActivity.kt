c56273eee906c3572f9abbe2d0b68f4b6cca661c
Yerlan Jumabayev <thevarp18@gmail.com>
2023-11-10 22:25:08 +0600

    init func

--- app/src/main/java/com/example/grid/MainActivity.kt
+++ app/src/main/java/com/example/grid/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.grid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.data.EmptyGroup.name
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.grid.ui.theme.GridTheme
 
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TopicGrid()
                 }
             }
         }
@@ -30,17 +35,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TopicGrid(modifier: Modifier = Modifier) {
+
 }
+@Composable
+fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    GridTheme {
-        Greeting("Android")
-    }
+fun TopicPreview() {
+
 }
